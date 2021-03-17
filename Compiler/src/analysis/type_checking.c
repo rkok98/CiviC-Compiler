ce70b630c94bc94619172b6f4c763905b40b9e36
René Kok <rkok98@gmail.com>
2021-03-17 14:10:48 +0100

    Program type checking

--- Compiler/src/analysis/type_checking.c
+++ Compiler/src/analysis/type_checking.c
@@ -8,21 +8,57 @@
 #include "str.h"
 #include "memory.h"
 
-node *TCprogram(node *arg_node, info *arg_info) {
+struct INFO
+{
+    node *symbol_table;
+};
+
+#define INFO_SYMBOL_TABLE(n) ((n)->symbol_table)
+
+static info *MakeInfo()
+{
+    info *result;
+
+    DBUG_ENTER("MakeInfo");
+
+    result = (info *)MEMmalloc(sizeof(info));
+    INFO_SYMBOL_TABLE(result) = NULL;
+
+    DBUG_RETURN(result);
+}
+
+static info *FreeInfo(info *info)
+{
+    DBUG_ENTER("FreeInfo");
+
+    info = MEMfree(info);
+
+    DBUG_RETURN(info);
+}
+
+node *TCprogram(node *arg_node, info *arg_info)
+{
     DBUG_ENTER("TCprogram");
+
+    INFO_SYMBOL_TABLE(arg_info) = PROGRAM_SYMBOLTABLE(arg_node);
+    PROGRAM_DECLS(arg_node) = TRAVdo(PROGRAM_DECLS(arg_node), arg_info);
 
     DBUG_RETURN(arg_node);
 }
 
 node *TCdoTypeChecking(node *syntaxtree)
 {
+    info *arg_info;
+
     DBUG_ENTER("TCdoTypeChecking");
 
-    TRAVpush(TR_tc); 
+    arg_info = MakeInfo();
 
+    TRAVpush(TR_tc);
     syntaxtree = TRAVdo(syntaxtree, NULL);
-
     TRAVpop();
+
+    arg_info = FreeInfo(arg_info);
 
     DBUG_RETURN(syntaxtree);
 }
