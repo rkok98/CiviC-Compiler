03ebde86cf172c06b1cc9d9dce50bc03bcab65a9
René Kok <rkok98@gmail.com>
2021-04-01 16:03:27 +0200

    H file

--- Compiler/src/analysis/symbol_table.h
+++ Compiler/src/analysis/symbol_table.h
@@ -1,12 +1,12 @@
-#ifndef _SYMBOL_TABLE_
-#define _SYMBOL_TABLE_
+#ifndef _SYMBOL_TABLE_H_
+#define _SYMBOL_TABLE_H_
 
 #include "types.h"
 
-extern node *STinsert(node *symbol_table, node *entry);
-
 extern unsigned int STcountParams(node *entry);
 extern unsigned int STcountVarDecls(node *entry);
+
+extern node *STinsert(node *symbol_table, node *entry);
 
 extern node *STfind(node *symbol_table, char *name);
 extern node *STfindInParents(node *symbol_table, char *name);
